d6f7f95dbb8f10f9159bf7d3603b60600689cb34
shuyu <46919489@qq.com>
2025-02-26 13:52:34 +0800

    initial project

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace avaloniaExample.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace avaloniaExample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
