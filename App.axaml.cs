335c7143e3cd317b91373fd2f59f1cb34062f8ea
shuyu <46919489@qq.com>
2025-02-27 11:10:06 +0800

    splash window

--- App.axaml.cs
+++ App.axaml.cs
@@ -9,6 +9,7 @@ using avaloniaExample.Views;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using avaloniaExample.ViewModels.SplitViewPane;
+using System.Threading.Tasks;
 
 namespace avaloniaExample;
 
@@ -21,7 +22,7 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
         InitializeServiceProvider();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
@@ -29,10 +30,36 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
+
+
+            // Adding custom splashScreen here
+            // for fluent splashScreen, refer to https://www.youtube.com/watch?v=-Ii4QmcYQUU
+            var splashSceenVm = new CustomSplashScreenViewModel();
+            var splashScreen = new CustomSplashScreenView {
+                DataContext = splashSceenVm
+            };
+            desktop.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            try {
+                splashSceenVm.StartupMessage = "Searching for devices...";
+                await Task.Delay(1000, splashSceenVm.CancellationToken);
+                splashSceenVm.StartupMessage = "Loading data...";
+                await Task.Delay(1000, splashSceenVm.CancellationToken);
+                splashSceenVm.StartupMessage = "Configuring device...";
+                await Task.Delay(1000, splashSceenVm.CancellationToken);
+            } 
+            catch (TaskCanceledException) {
+                splashScreen.Close();
+                return;
+            }
+
             var viewModel = ServiceProvider?.GetService<MainViewModel>();
             if (viewModel != null) 
             {
                 desktop.MainWindow = new MainWindow(viewModel);
+                desktop.MainWindow.Show();
+                splashScreen.Close();
             }
             else
             {
